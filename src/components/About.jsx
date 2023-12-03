d3d01442c7d681d80858ff6e2326c1c0bc7d85ac
gk30074 <guy30074@gmail.com>
2023-12-03 18:06:33 +0200

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './About.css'
+
+export const About = () => {
+  return (
+    <div className='container'>
+      <h1 className='title'>Hi My Name is Guy</h1>
+      <span className='description'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus dignissimos, dolorum velit hic culpa, magnam quibusdam deleniti impedit laudantium perspiciatis sit eligendi voluptate iure sapiente veritatis animi placeat veniam officiis.
+      </span>
+    </div>
+  )
+}
